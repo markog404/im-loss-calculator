c61212cb53dc9ab4a856db34777c1b699e40a5fd
markog404 <87530551+markog404@users.noreply.github.com>
2021-12-18 14:55:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -15,10 +15,9 @@ function Calculate() {
   nothing.innerHTML = "";
   
   
-    const profit = (((BI*AF+AI*BF)-(Math.sqrt((AI * BI*BF)/AF)*AF+Math.sqrt((AI * BI*AF)/BF)*BF))/(BI*AF+AI*BF)*100).toFixed(4);
+    const profit = parseFloat((((BI*AF+AI*BF)-(Math.sqrt((AI * BI*BF)/AF)*AF+Math.sqrt((AI * BI*AF)/BF)*BF))/(BI*AF+AI*BF)*100).toFixed(4));
     const profit_lp = parseFloat(((1/(2*AI*BI))*(Math.sqrt((AI * BI*BF)/AF)*AF+Math.sqrt((AI * BI*AF)/BF)*BF)).toFixed(4));
-  
-      const profit_hodl = parseFloat(((1/(2*AI*BI))*(BI*AF+AI*BF)).toFixed(4));
+    const profit_hodl = parseFloat(((1/(2*AI*BI))*(BI*AF+AI*BF)).toFixed(4));
   
     impermanent__loss.innerHTML = "Impermanent loss : " + profit +"%";
     expected__value.innerHTML = " Expceted value for every $1 invested: <br>"
